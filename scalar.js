2974440071ef5d12278f8ed74d3d66acb8805163
Sergii Kruk <54427100+Sergej70@users.noreply.github.com>
2020-07-31 08:49:29 +0300

    Update scalar.js

--- scalar.js
+++ scalar.js
@@ -5,3 +5,15 @@ scalar1--; // 4
 scalar2++; // 6
 
 console.dir({ scalar1, scalar2 });  // Object { scalar1:4, scalar2:6 };
+
+const object1 = { field: 6 };
+const object2 = object1;
+object1.field = 6;
+
+console.log({ object1, object2 });
+/*
+{object1: {…}, object2: {…}}
+object1: {field: 6}
+object2: {field: 6}
+__proto__: Object
+*/
