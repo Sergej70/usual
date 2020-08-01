5dee9d460943ac9c08aa68c79a5e32b257bb1de3
Sergii Kruk <54427100+Sergej70@users.noreply.github.com>
2020-08-01 07:38:15 +0300

    Create types.js

---
+++ types.js
@@ -0,0 +1,19 @@
+'use stict';
+
+const i = 8;
+const f = 12.4;
+const s = 'Hello';
+const b = true;
+
+const person = {
+    name: 'Sergii Kruk',
+    born: 70,
+    city: 'Herson',
+    position: 'man'
+}
+
+//console.dir(person.city);  //Herson
+
+person.city = 'Kokchetav';
+
+//console.log(person.city);  //Kokchetav
