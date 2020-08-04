2eae3b09d8712909934173df5fe4837ca7cda93c
Sergii Kruk <sergo05a@gmail.com>
2020-08-04 19:25:56 +0300

    Update types.js

--- types.js
+++ types.js
@@ -18,6 +18,13 @@ person.city = 'Kokchetav';
 
 //console.log(person.city);  //Kokchetav
 
+const cities = ['Kiev', 'Herson', 'Minsk'];
+
+cities.unshift('Mytischi');
+cities.push('Granada');
+
+console.log(cities);   //  ["Mytishi", "Kiev", "Herson", "Minsk", "Granada"]; 
+
 console.log('shift: ' + cities.shift());  // shift: Mytischi
 console.log('pop: ' + cities.pop());      // pop: Granada 
 
@@ -25,3 +32,11 @@ console.log({ i }, typeof i);  // {i: 8} "number"
 console.log({ f }, typeof f);  // {f: 12.4} "number"
 console.log({ s }, typeof s);  // {s: "Hello"} "string"
 console.log({ b }, typeof b);  // {b: true} "boolean
+
+console.log({ person }, typeof person);   // {person: {...}} "object"
+console.log({ isArray: Array.isArray(person) });  // {isArray: false}
+
+console.log({ cities }, typeof cities);   // {cities: Array()} "object"
+console.log({ isArray: Array.isArray(cities) });   // {isArray: true}
+
+console.log({ instanceofArray: cities instanceof Array });  // {instanceofArray: true}
