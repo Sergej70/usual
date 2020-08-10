f751b3deee4e01f75e67acd6989c99f963edb354
Sergii Kruk <sergo05a@gmail.com>
2020-08-10 08:13:02 +0300

    Update bigint.js

--- bigint.js
+++ bigint.js
@@ -22,3 +22,53 @@ console.log('if (max === biSafe) :', max === biSafe);
   // if (max === biSafe) : false
 console.log('if (max == biSafe) :', max == biSafe);
   // if (max == biSafe) : true
+
+console.log('biSafe      =', biSafe);
+  // biSafe      = 4503599627370495n
+console.log('biSafe + 1n =', biSafe + 1n);
+  // biSafe + 1n = 4503599627370496n
+console.log('biSafe + 2n =', biSafe + 2n);
+  // biSafe + 2n = 4503599627370497n
+console.log('');
+
+console.log('15n / 3n =', 15n / 3n);
+  // 15n / 3n = 5n
+console.log('3n / 2n =', 3n / 2n);
+  // 3n / 2n = 1n
+console.log('BigInt(3n / 2n) =', BigInt(3n / 2n));
+  // BigInt(3n / 2n) = 1n
+  console.log('');
+
+//console.log('BigInt(\'1.5\') =', BigInt('1.5'));
+  // SyntaxError: Cannot convert 1.5 to a BigInt 
+  // at BigInt (<anonymous>)
+
+//console.log('BigInt(1.5) =', BigInt(1.5));
+  // RangeError: The number 1.5 cannot be converted to a BigInt 
+  // because it is not an integer
+console.log('');
+
+console.log('2n > 1n =', 2n > 1n);
+  // 2n > 1n = true
+console.log('2n > 1 =', 2n > 1);
+  // 2n > 1 = true
+console.log('');
+
+console.log('(1000n ** 200n) / 12321n =', (1000n ** 20n) / 12321n ** 10n);
+  // (1000n ** 200n) / 12321n = 12403390708964314479n
+  console.log('');
+
+const array1 = [-2, 7, 1n, 3, -2n, 8n, 5, -4n];
+console.log(array1);
+  // (8) [-2, 7, 1n, 3, -2n, 8n, 5, -4n]
+console.log(array1.sort());
+  // (8) [-2, -2n, -4n, 1n, 3, 5, 7, 8n]
+console.log('');
+
+const array2 = [3, -2, 7n, -5, -1n, 2, 5, 0];
+console.log(array2);
+  // (8) [3, -2, 7n, -5, -1n, 2, 5, 0]
+console.log(array2.sort());
+  // (8) [-1n, -2, -5, 0, 2, 3, 5, 7n]
+
+  // https://github.com/tc39/proposal-bigint
